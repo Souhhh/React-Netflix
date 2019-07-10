c82c331b4bb4605de036c8ac1ccfe17c6d28ac6a
Souhaila <so.ouakrim@gmail.com>
2019-07-10 11:15:24 +0200

    Carousel : il y a maintenant 2 carousels (un pour chaque catégorie de films) mais les 2 carousels sont liés donc quand je clique sur la flèche de droite du 2ième carousel, ça avance celui du dessus (donc pas le bon).

--- src/containers/video-list.js
+++ src/containers/video-list.js
@@ -19,44 +19,26 @@ class VideoList extends Component {
     
         render() {
             return (
-        <div>
-            {/* LISTE DES FILMS LES PLUS POPULAIRES */}
-            <h4 className="titreSection">{this.props.title}</h4> 
-            <div className="container-videoList">
-                <a href={`#${this.state.previous}`} className="arrow__btn" onClick={this.scrollLeft.bind(this)}>‹</a>
-                <ul className="videos">
-                {/* Pour chaque vidéo recommandée, on lui donne un id pour ensuite se référer à lui pour fiare tourner le carousel. */}
-                {this.state.movieList.map((movie, index) => {
-                
-                        return <div id ={index} ><VideoListItem key={movie.id} movie={movie} callback={this.onClickListItem.bind(this)}/></div>
-                    })}
-                </ul>
-                <a href={`#${this.state.next}`} className="arrow__btn" onClick={this.scrollRight.bind(this)}>›</a>
-            </div>
-            <div>
-
-            {/* LISTE DES FILMS TYPE COMÉDIE */}
-            {/* <h4 className="titreSection">Comedy</h4>
-            <div className="container-videoList">
-                <a href={`#${this.state.previous}`} className="arrow__btn" onClick={this.scrollLeft.bind(this)}>‹</a>
-                <ul className="videos">
-                {/* Pour chaque vidéo recommandée, on lui donne un id pour ensuite se référer à lui pour fiare tourner le carousel. */}
-                {/* {this.state.moviesComedy.map((movie, index) => { */}
-                
-                        {/* return <div id ={index} ><VideoListItem key={movie.id} movie={movie} callback={this.onClickListItem.bind(this)}/></div> */}
-                    {/* })} */}
-                {/* </ul> */}
-                {/* <a href={`#${this.state.next}`} className="arrow__btn" onClick={this.scrollRight.bind(this)}>›</a> */}
-            {/* </div> */} 
-            
-            {/* LISTE DES DOCUMENTAIRES */}
-            {/* <h4 className="titreSection">Documentary</h4> */}
-            
-
-            {/* LISTE DES FILMS D'HORREUR */}
-            {/* <h4 className="titreSection">Horror</h4> */}
-        </div>
-        </div>
+                <div>
+                    {/* LISTE DES FILMS LES PLUS POPULAIRES */}
+                    <h4 className="titreSection">{this.props.title}</h4> 
+                    <div className="container-videoList banner_fluid">
+                        <div className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">                            
+                                <ul className="videos">
+                                {/* Pour chaque vidéo recommandée, on lui donne un id pour ensuite se référer à lui pour fiare tourner le carousel. */}
+                                {this.state.movieList.map((movie, index) => {                            
+                                        return <div id ={index} ><VideoListItem key={movie.id} movie={movie} callback={this.onClickListItem.bind(this)}/></div>
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="carousel-control">                               
+                                <a href={`#${this.state.previous}`} className="arrow__btn left" onClick={this.scrollLeft.bind(this)}>‹</a>                               
+                                <a href={`#${this.state.next}`} className="arrow__btn right" onClick={this.scrollRight.bind(this)}>›</a>                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
         
     );
